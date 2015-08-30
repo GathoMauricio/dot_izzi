8c0ac8fb5663f7962df2250477045355800b23aa
GathoMauricio <mauricio2769@gmail.com>
2015-08-30 02:10:52 -0500

    validando

--- drwifi/validar_expediente.php
+++ drwifi/validar_expediente.php
@@ -2,4 +2,15 @@
 header('Access-Control-Allow-Origin: *');
 include "../mobile/conexion.php";
 $consulta="SELECT * FROM expediente WHERE id_expediente=".$_POST['expediente']. AND id_estatus=1;
+$datos=mysqli_query($conexion,$consulta);
+if($fila=mysqli_fetch_array($datos))
+{
+	$json=array(
+		"expediente"=>$fila['id_expediente'],
+		"usuario"=>$fila['usuario']
+		);
+}else
+{
+	echo false;
+}
  ?>
