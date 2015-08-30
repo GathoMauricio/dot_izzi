0b36c216622dadcc20cd44763dbbf9bdd8c2d35b
GathoMauricio <mauricio2769@gmail.com>
2015-08-30 02:13:40 -0500

    qwery

--- drwifi/validar_expediente.php
+++ drwifi/validar_expediente.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 include "../mobile/conexion.php";
-$consulta="SELECT * FROM expediente WHERE id_expediente=".$_POST['expediente']. AND id_estatus=1;
+$consulta="SELECT * FROM expediente WHERE id_expediente=".$_POST['expediente']." AND id_estatus=1";
 $datos=mysqli_query($conexion,$consulta);
 if($fila=mysqli_fetch_array($datos))
 {
