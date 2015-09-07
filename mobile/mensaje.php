a205700fdfd7bb60fb2c50be99386c3b9770a37c
GathoMauricio <mauricio2769@gmail.com>
2015-09-06 23:58:58 -0500

    msg

--- mobile/mensaje.php
+++ mobile/mensaje.php
@@ -5,7 +5,7 @@ $pusher = PusherInstance::get_pusher();
 $pusher->trigger(
 'dot_mensaje',
 'mensaje'.$_POST['id'],
-array('mensaje' => "Favor de actualizar la aplicación!!!")
+array('mensaje' => "Favor de actualizar la aplicación!!! \nEn caso de tener la app ya actualizada hacer caso omiso")
 );
 echo "Alerta enviada!!!";
  ?>
