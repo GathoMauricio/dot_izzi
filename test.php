2feaa2a9e9cf0677ba82ab7a3f15d337c5b63c3a
GathoMauricio <mauricio2769@gmail.com>
2015-09-17 15:05:08 -0500

    test

--- test.php
+++ test.php
@@ -1,9 +1,12 @@
 <?php 
-//echo "<BR>".$_FILES["imagen"]["name"];      //nombre del archivo
-//echo "<BR>".$_FILES["imagen"]["type"];      //tipo
-//echo "<BR>".$_FILES["imagen"]["tmp_name"];  //nombre del archivo de la imagen temporal
-//echo "<BR>".$_FILES["imagen"]["size"];      //tamaño
-echo "EXP: ".$_POST['expediente'];
-
-//move_uploaded_file($_FILES['imagen']['tmp_name'], 'test/'.$_FILES['imagen']['name'])
+function getRealIP() {
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))
+        return $_SERVER['HTTP_CLIENT_IP'];
+       
+    if (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+        return $_SERVER['HTTP_X_FORWARDED_FOR'];
+   
+    return $_SERVER['REMOTE_ADDR'];
+}
+echo 'IP = '.getRealIP();
  ?>
